80625882653f5189bce114033a3415bf57f22635
asultan00 <asultan00@stuy.edu>
2019-12-22 11:21:42 -0500

    added import stuff and began a database thing

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask
+from flask import render_template
+from flask import redirect
+from flask import url_for
+from flask import session
+from flask import flash
+import sqlite3
+import os
+import json
 
 app = Flask(__name__)
+
+# setting up the database
+DB_FILE = "deutschebank.db"
+db = sqlite3.connect(DB_FILE, check_same_thread=False)
+c = db.cursor()
 
 @app.route("/")
 def root():
