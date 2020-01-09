48e5775572aa2b3fd1eec29ea8430f131059eebd
Kazi Jamal <jamalkazi02@gmail.com>
2020-01-09 11:07:11 -0500

    rounded temperature

--- utl/api.py
+++ utl/api.py
@@ -5,7 +5,7 @@ def getCurrentWeather():
     weatherlink = "https://www.metaweather.com/api/location/2459115/"
     weatherjson = request.urlopen(weatherlink).read()
     weather = json.loads(weatherjson)['consolidated_weather'][0]
-    weather['min_temp'] = weather['min_temp'] * 9.0 / 5.0 + 32
-    weather['max_temp'] = weather['max_temp'] * 9.0 / 5.0 + 32
-    weather['the_temp'] = weather['the_temp'] * 9.0 / 5.0 + 32
+    weather['min_temp'] = int(weather['min_temp'] * 9.0 / 5.0 + 32)
+    weather['max_temp'] = int(weather['max_temp'] * 9.0 / 5.0 + 32)
+    weather['the_temp'] = int(weather['the_temp'] * 9.0 / 5.0 + 32)
     return weather
